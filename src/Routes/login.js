ffad6c5508269e1b56abc634e662728e0385204d
isadiazac <isadiac06@gmail.com>
2024-09-26 21:17:19 -0500

    changes

--- src/Routes/login.js
+++ src/Routes/login.js
@@ -5,8 +5,8 @@ const jwt = require("jsonwebtoken");
 let lista = require("../Modelos/ListaLogin");
 
 //TRAE EL EMAIL Y CONTRASEÑA DE LISTALOGIN.JSON. NO SIRVIÓ
-router.get("/login", (req, res) => {
-  res.json(lista);
+router.get("/login", authenticateToken, (req, res) => {
+  res.json(lista.filter((post) => post.user === req.email.name));
 });
 
 /*Esto es para autenticar el usuario si la lista estuviera desocupada haciendo un post, pero le puse la info
@@ -19,3 +19,15 @@ router.post("/login", (req, res) => {
   const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
   res.json({ accessToken: accessToken });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
